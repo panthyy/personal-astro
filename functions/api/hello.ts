abc5df9ac8309f08ef1409ea2e88d59d09960910
panthyy <73543613+panthyy@users.noreply.github.com>
2022-07-09 07:08:51 +0000

    Deploying to gh-pages from @ panthyy/personal-astro@8f8665b1252880c1e7d260232feda6434ea79e25 🚀

---
+++ functions/api/hello.ts
@@ -0,0 +1,25 @@
+export async function onRequestGet(request:Request) {
+    const dateToday = new Date()
+
+    const html = `
+    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Current time ${dateToday}</h1>
+</body>
+</html>
+    `
+    
+    return new Response(html,{
+        "headers" :{
+            "Content-Type": "text/html"
+        }
+    })
+}
