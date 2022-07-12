f90fd4ef205b4e42847ead65145b104cb295c05f
Panthy <fahad.alhatshami@hotmail.com>
2022-07-12 19:31:38 +0200

    dont judge me

--- src/components/Base/index.jsx
+++ src/components/Base/index.jsx
@@ -1,12 +1,16 @@
 import "../../styles/global.css";
-function Base({ children }) {
+import anime from "animejs";
+import { useRef, useState, useEffect } from "react";
+const Base = ({ children }) => {
   return (
     <div
       id="Base"
       className=" dark:text-[#FFFFFF] dark:bg-[#111111] text-[#1A1A1A] flex px-8 sm:px-20  justify-center w-full min-h-full "
     >
-      <div className="Background  w-full sm:max-w-[1100px] ">{children}</div>
+      <div id="Children" className=" Background  w-full sm:max-w-[1100px] ">
+        {children}
+      </div>
     </div>
   );
-}
+};
 export { Base };
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,72 +1,65 @@
-import { useTheme } from 'next-themes'
+import { useTheme } from "next-themes";
 function Header() {
-    const { theme, setTheme } = useTheme()
-    const menuItems = [
-        {
-            href: "#Projects",
-            text: "Projects"
-        },
-        {
-            href:"#Resume",
-            text: "Resume"
-        },
-        {
-            href:"/blog",
-            text: "Blog"
-        },
-        {
-            href:"#Contact",
-            text: "Contact"
-        }
-    ]
+  const { theme, setTheme } = useTheme();
+  const menuItems = [
+    {
+      href: "#Projects",
+      text: "Projects",
+    },
+    {
+      href: "#Resume",
+      text: "Resume",
+    },
+    {
+      href: "/blog",
+      text: "Blog",
+    },
+    {
+      href: "#Contact",
+      text: "Contact",
+    },
+  ];
 
-    return (
-        <header className="  h-[123px] mt-4  flex items-center justify-center mb-[50px] sm:mb-[133px] ">
-            <div className="w-full  ">
-                <div className=" flex h-[29px] justify-between  ">
-                    <div className="text-[18px] font-[Inter] ">
-                       <a className='' href='/'>
-                       BSCE Student at Lund university
-                          </a>
-                    </div>
-                    <nav className=" flex Nav gap-7 items-center justify-center ">
-                        <ul className="text-[18px] hidden sm:flex gap-7 group-hover:cursor-pointer align-middle gap">
-
-                            <ul className="text-[18px] hidden sm:flex gap-7 group-hover:cursor-pointer align-middle gap">
-                            {menuItems.map((item, index) => {
-                                return (
-                                    <li key={index}>
-                                        <a href={item.href}>{item.text}</a>
-                                    </li>
-                                )
-                            })}
-    
-                        </ul>
-                        </ul>
-                        <button
-                            onClick={() =>
-                                setTheme(theme === 'dark' ? 'light' : 'dark')
-                            }
-                            className=" "
-                        >
-                        
-                        </button>
-                    </nav>
-                </div>
-                <nav className=" flex Nav gap-7 items-center justify-center ">
-                    <ul className="text-[18px] mt-10 justify-evenly w-full sm:hidden flex  group-hover:cursor-pointer align-middle gap">
-                        {menuItems.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    <a href={item.href}>{item.text}</a>
-                                </li>
-                            )
-                        }
-                        )}
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="  h-[123px] mt-4  flex items-center justify-center mb-[50px] sm:mb-[133px] ">
+      <div className="w-full  ">
+        <div className=" flex h-[29px] justify-between  ">
+          <div className="text-[18px] font-[Inter] ">
+            <a className="" href="/">
+              BSCE Student at Lund university
+            </a>
+          </div>
+          <nav className=" flex Nav gap-7 items-center justify-center ">
+            <ul className="text-[18px] hidden sm:flex gap-7 group-hover:cursor-pointer align-middle gap">
+              <ul className="text-[18px] hidden sm:flex gap-7 group-hover:cursor-pointer align-middle gap">
+                {menuItems.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <a href={item.href}>{item.text}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </ul>
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              className=" "
+            ></button>
+          </nav>
+        </div>
+        <nav className=" flex Nav gap-7 items-center justify-center ">
+          <ul className="text-[18px] mt-10 justify-evenly w-full sm:hidden flex  group-hover:cursor-pointer align-middle gap">
+            {menuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a href={item.href}>{item.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
-export { Header }
+export { Header };
